791c599855d88b3c97668fa3b783e47887059f65
Bojan Piskulić <bpiskulic@qubitsoft.rs>
2024-02-16 23:16:31 +0100

    Add tests

--- test/OpenPlanningPoker.GameEngine.Domain.UnitTests/GlobalUsings.cs
+++ test/OpenPlanningPoker.GameEngine.Domain.UnitTests/GlobalUsings.cs
@@ -4,5 +4,7 @@ global using FluentAssertions;
 global using OpenPlanningPoker.GameEngine.Domain.Abstractions;
 global using OpenPlanningPoker.GameEngine.Domain.Games;
 global using OpenPlanningPoker.GameEngine.Domain.Games.Events;
+global using OpenPlanningPoker.GameEngine.Domain.Tickets;
+global using OpenPlanningPoker.GameEngine.Domain.Tickets.Events;
 global using OpenPlanningPoker.GameEngine.Domain.UnitTests.Infrastructure;
 global using Xunit;
