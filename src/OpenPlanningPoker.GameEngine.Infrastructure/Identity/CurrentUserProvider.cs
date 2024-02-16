9d3df164ae886f39fc6c4024e0f2474e9b405ef5
Bojan Piskulić <bpiskulic@qubitsoft.rs>
2024-02-16 23:39:08 +0100

    Add dummy guid for current user provider.

--- src/OpenPlanningPoker.GameEngine.Infrastructure/Identity/CurrentUserProvider.cs
+++ src/OpenPlanningPoker.GameEngine.Infrastructure/Identity/CurrentUserProvider.cs
@@ -2,5 +2,5 @@
 
 public class CurrentUserProvider : ICurrentUserProvider
 {
-    public Guid CustomerId { get; } = new(); // TODO: Implement this!
+    public Guid CustomerId { get; } = new("70d6c641-6299-4cb4-9f39-97c180115a34"); // TODO: Implement this!
 }
