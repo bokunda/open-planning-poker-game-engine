30c7e8afe7d71905af6b72e5bb17df247fdc4dd9
Bojan Piskulić <bpiskulic@qubitsoft.rs>
2024-02-16 23:29:36 +0100

    Support created by to all entities

--- src/OpenPlanningPoker.GameEngine.Domain/Abstractions/Entity.cs
+++ src/OpenPlanningPoker.GameEngine.Domain/Abstractions/Entity.cs
@@ -14,6 +14,8 @@ public abstract class Entity<TEntityId> : IEntity, IEntityHasCreated
 #pragma warning restore CS8618
 
     public TEntityId Id { get; init; }
+    public Guid CreatedBy { get; private set; }
+
     public DateTimeOffset CreatedOn { get; private set; }
     public bool IsDeleted { get; private set; }
 
@@ -21,5 +23,9 @@ public abstract class Entity<TEntityId> : IEntity, IEntityHasCreated
     public IReadOnlyList<IDomainEvent> GetDomainEvents() => _domainEvents.ToList();
     public void ClearDomainEvents() => _domainEvents.Clear();
     protected void RaiseDomainEvent(IDomainEvent domainEvent) => _domainEvents.Add(domainEvent);
-    public void SetCreated(DateTimeOffset createdOn) => CreatedOn = createdOn;
+    public void SetCreated(DateTimeOffset createdOn, Guid createdBy)
+    {
+        CreatedOn = createdOn;
+        CreatedBy = createdBy;
+    }
 }
---
+++ src/OpenPlanningPoker.GameEngine.Infrastructure/Migrations/20240216222318_AddCreatedByToAllEntities.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenPlanningPoker.GameEngine.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreatedByToAllEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "created_by",
+                table: "votes",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "created_by",
+                table: "tickets",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "created_by",
+                table: "gamesettings",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "created_by",
+                table: "games",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "created_by",
+                table: "audits",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "created_by",
+                table: "votes");
+
+            migrationBuilder.DropColumn(
+                name: "created_by",
+                table: "tickets");
+
+            migrationBuilder.DropColumn(
+                name: "created_by",
+                table: "gamesettings");
+
+            migrationBuilder.DropColumn(
+                name: "created_by",
+                table: "games");
+
+            migrationBuilder.DropColumn(
+                name: "created_by",
+                table: "audits");
+        }
+    }
+}
